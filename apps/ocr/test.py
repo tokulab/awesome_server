9a29401ef2d8f3d78673aba73ab7629ed79bc62d
梅宮 <euthanasia045@gmail.com>
2019-04-22 16:52:34 +0900

    WIP post raw img from curl.

--- apps/ocr/test.py
+++ apps/ocr/test.py
@@ -1,6 +1,6 @@
 from ocr_api import Ocr
 
-ocr = Ocr()
+ocr = Ocr(config_path='./config.yaml')
 ocr.read_img(input())
 ocr.img_effect('gray', show=True)
 str = ocr.img_to_str(lang='jpn', tesseract_layout=6)
